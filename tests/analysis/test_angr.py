c39fd523e0ad853732cede530bd238fb7f931434
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2022-09-14 19:07:20 -0400

    Add new unit tests

---
+++ tests/analysis/test_angr.py
@@ -0,0 +1,20 @@
+"""
+test_angr.py
+"""
+import unittest
+from pathlib import Path
+
+from fuzzable.analysis.angr import AngrAnalysis 
+
+class TestAngrAnalysis(unittest.TestCase):
+    def test_basic(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_angr_analysis(self):
+        analyzer = AngrAnalysis()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_generate.py
@@ -0,0 +1,20 @@
+"""
+test_generate.py
+"""
+import unittest
+from pathlib import Path
+
+from fuzzable import generate 
+
+class TestHarnessGen(unittest.TestCase):
+    def test_basic(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_transform_elf_to_so(self):
+        generate.transform_elf_to_so()
+
+
+if __name__ == '__main__':
+    unittest.main()
